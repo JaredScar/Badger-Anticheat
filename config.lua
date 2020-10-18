2424d8b5b5f16a55d9c6816f45edfae3966793d4
JamesUK <60899490+JamesUK-Developer@users.noreply.github.com>
2020-10-18 13:21:41 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,7 +12,7 @@ Config = {
          AntiRemoveOtherPlayersWeapons = false, -- Stops players removing other players weapons. 
          AntiCancelAnimations = false, -- Stops other players cancelling other people's animations (kicking people out of cars etc.)
          StopOtherPlayersGivingEachOtherWeapons = false, -- Stops other players giving other people weapons (If you have an admin menu to give weapons this may be an issue. Giving weapons with esx inventories or etc will not cause any issues!)
-         ModMenuChecks = true, -- Will check in your resources for any injected code being variables/functions that has been added to our detection lists! Don't forget to also enable it on the client script! In acloader.lua! 
+         ModMenuChecks = false, -- Will check in your resources for any injected code being variables/functions that has been added to our detection lists! Don't forget to also enable it on the client script! In acloader.lua! 
          StopUnauthorizedResources = false, -- Will check for resources that have not been allowed by the server! (Common with Eulen tick box menu loading options)
     },
     BanComponents = {
